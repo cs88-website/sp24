538aa1c4c5862e233001659d33097ed362c0ba42
Michael Ball <michael@mball.co>
2024-01-19 16:41:09 -0800

    Deploy website Fri Jan 19 16:41:07 PST 2024

---
+++ lab/lab01/lab01_extra.py
@@ -0,0 +1,49 @@
+"""Optional questions for Lab 1"""
+
+# While Loops
+
+def triangular_sum(n):
+    """
+    >>> t_sum = triangular_sum(5)
+    1
+    3
+    6
+    10
+    15
+    >>> t_sum
+    35
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+
+# Boolean Operators
+
+def right_triangle(a, b, c):
+    """Determine whether a, b, and c can be sides of a right triangle
+    >>> right_triangle(1, 1, 1)
+    False
+    >>> right_triangle(5, 3, 4)
+    True
+    >>> right_triangle(8, 10, 6)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
