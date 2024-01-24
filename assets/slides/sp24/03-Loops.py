3f57798f50224c5935228ccda57cbcb39a2d9b8f
Michael Ball <michael@mball.co>
2024-01-24 13:59:07 -0800

    Deploy website Wed Jan 24 13:59:06 PST 2024

---
+++ assets/slides/sp24/03-Loops.py
@@ -0,0 +1,14 @@
+
+course = 'C88C'
+time = '2:00'
+if time == '2:00':
+    print(f"Go to {course}")
+else:
+    print("Go get some ☕️")
+
+# Go to C88C
+
+
+
+temp = 70
+status = "it's hot!" if temp > 85 else 'not hot…'
